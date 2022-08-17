7629a1760ef262062e2fe1e383bfb3b164fadf8f
nahuelfd <nahuelfd@gmail.com>
2022-08-17 20:11:44 -0300

    agregado de emailjs al contactform

---
+++ js/contacto.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('button');
+
+    document.getElementById('form')
+     .addEventListener('submit', function(event) {
+       event.preventDefault();
+    
+       btn.value = 'Enviando...';
+    
+       const serviceID = 'default_service';
+       const templateID = 'template_01ud9zg';
+    
+       emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+          btn.value = 'Enviar';
+          alert('Mensaje enviado con éxito!'); 
+          location.reload();
+        }, (err) => {
+          btn.value = 'Enviar';
+          alert(JSON.stringify(err));
+        });
+    });
+
+    
--- js/reservas.js
+++ js/reservas.js
@@ -104,5 +104,7 @@ window.onload=function () {
       //  }
    // })
 
-
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json));
 }
